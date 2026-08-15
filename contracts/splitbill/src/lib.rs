259a49a59d2410114b1724d524d6315d46043bc2
yyred-max <anay29758@gmail.com>
2026-08-15 11:09:25 +0700

    feat: implement create_group with owner auth

--- contracts/splitbill/src/lib.rs
+++ contracts/splitbill/src/lib.rs
@@ -30,6 +30,39 @@ const MEMBER_DATA: Symbol = symbol_short!("MEMBERS");
 pub struct SplitBillContract;
 
 #[contractimpl]
-impl SplitBillContract {}
+impl SplitBillContract {
+    pub fn get_groups(env: Env) -> Vec<Group> {
+        env.storage()
+            .instance()
+            .get(&GROUP_DATA)
+            .unwrap_or(Vec::new(&env))
+    }
+
+    pub fn create_group(env: Env, owner: Address, name: String) -> Group {
+        owner.require_auth();
+
+        let mut groups: Vec<Group> = env
+            .storage()
+            .instance()
+            .get(&GROUP_DATA)
+            .unwrap_or(Vec::new(&env));
+
+        let group = Group {
+            id: env.prng().gen::<u64>(),
+            name,
+            owner: owner.clone(),
+            total_members: 0,
+            members_paid: 0,
+        };
+
+        groups.push_back(group.clone());
+        env.storage().instance().set(&GROUP_DATA, &groups);
+
+        env.events()
+            .publish((symbol_short!("g_create"), group.id), owner);
+
+        group
+    }
+}
 
 mod test;
